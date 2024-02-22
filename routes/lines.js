96a31b46d152d3723cee8808a6cd0d0640056f1e
OneOTF2024 <160819627+OneOTF2024@users.noreply.github.com>
2024-02-22 13:07:07 +0700

    Add files via upload

--- routes/lines.js
+++ routes/lines.js
@@ -1,40 +1,10 @@
 const express = require("express");
-const LineDB = require("../model/line");
 const router = express.Router();
+const { ListLine, PostLine, GetLineId } = require("../controllers/lines");
 // hiển thị danh sách các dây
-router.get("/lines", (req, res, next) => {
-  LineDB.find({})
-    .then((data) => {
-      res.send(data);
-    })
-    .catch((err) => res.status(401).json({ err: "err", message: err.message }));
-});
+router.get("/lines", ListLine);
 // tạo dây mới
-router.post("/lines", (req, res, next) => {
-  const body = req.body;
-  LineDB.create({
-    price: body.price,
-    dateOpen: body.date,
-    people: body.people,
-    numberLine: body.line,
-    thao: body.thao,
-    idLine: body.idLine,
-  })
-    .then(() => {
-      res.status(200).json("thêm Dây mới thành công");
-    })
-    .catch((err) => res.status(401).json({ err: "err", message: err.message }));
-});
+router.post("/lines", PostLine);
 // tìm kiếm dây theo IdLine
-router.get("/lines/:id", (req, res, next) => {
-  const id = req.params.id;
-  console.log(id);
-  LineDB.find({
-    idLine: id,
-  })
-    .then((data) => {
-      res.status(200).json(data[0]);
-    })
-    .catch((err) => res.status(401).json({ err: "err", message: err.message }));
-});
+router.get("/lines/:id", GetLineId);
 module.exports = router;
